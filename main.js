bb824a6e4e97e3412c6beb8bb54be16a4ae02f18
paste94 <94paste94@gmail.com>
2018-12-04 11:03:41 +0100

    Modifiche alle razze

--- main.js
+++ main.js
@@ -40,13 +40,56 @@ function myFunction(){
                 $('#demo').text(data['status'] + ' ' + data['statusText']);
             }
         }
+    });
+}
+/*
+function tryit(url){
+	//alert(url);
+	jQuery.ajax({
+        dataType: 'json',
+        url: url,
+        context: document.body,
+        complete: (data) => {
+					if (data['status'] == 200){
+							var d = $.parseJSON(data['responseText']);
+						//alert(d.name);
+							$('#modal-race-header').text(d.name);
+						
+							$('#demo').text(JSON.stringify(d, null, '\t'));
+						$('#race-modal').modal('toggle');
+
+					}
+					else if (data['status'] == 404) {
+							$('#demo').text(data['status'] + ' ' + data['statusText']);
+					}
+        }
     })
-}
 
-function tryit(link){
-    alert(link);
+	
 }
+*/
 
+function tryit(url){
+	jQuery.ajax({
+        dataType: 'json',
+        url: url,
+        context: document.body,
+        complete: (data) => {
+					if (data['status'] == 200){
+							var d = $.parseJSON(data['responseText']);
+						//alert(d.name);
+							$('#class-name').text(d.name);
+							$('#speed').text(d.speed + ' ft (' + Math.floor(d.speed/3.3) +' mt)');
+							$('#demo').text(JSON.stringify(d, null, '\t'));
+						//$('#race-modal').modal('toggle');
+
+					}
+					else if (data['status'] == 404) {
+							$('#demo').text(data['status'] + ' ' + data['statusText']);
+					}
+        }
+    });
+}
 $('#races').ready(function(){
     var call_url = domain + 'races';
     jQuery.ajax({
@@ -64,6 +107,8 @@ $('#races').ready(function(){
                                     $('<td>').text(d.results[i].name)
                                 )
                             );
+                            
+                        //https://getbootstrap.com/docs/4.1/components/modal/#varying-modal-content
                         }else{
                             var $tr = $('#races-table').append(
                                 $('<tr role="row" class="even" onclick="tryit(\'' + d.results[i].url + '\')">').append(
@@ -72,14 +117,14 @@ $('#races').ready(function(){
                             );
                         }
                 });
-                $('#demo').text(JSON.stringify(d, null, '\t'));
+                //$('#demo').text(JSON.stringify(d, null, '\t'));
                 
             }
             else if (data['status'] == 404) {
                 $('#demo').text(data['status'] + ' ' + data['statusText']);
             }
         }
-    })
+    });
 });
 
 function request(){
